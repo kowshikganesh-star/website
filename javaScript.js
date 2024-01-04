28d514c6209314e28ad0046e4075c1eae4ea954f
kowshik <kowshikganesh2002@gmail.com>
2024-01-04 21:32:39 +0530

    changes happen

--- javaScript.js
+++ javaScript.js
@@ -1,8 +1,2 @@
 alert("welcome to TapAcademy");
-const login = document.getElementsByClassName("btn1");
-document.querySelector("#login").addEventListener(
-    "click",function(){
-       console.log("Login form");
-    },false
-);
 
