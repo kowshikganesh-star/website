fc6627090dd4452d2e7e6188c2fab0650dcf6a1c
kowshik <kowshikganesh2002@gmail.com>
2024-01-05 10:54:51 +0530

    changes made

--- javaScript.js
+++ javaScript.js
@@ -1,2 +1,5 @@
 alert("welcome to TapAcademy");
-
+document.querySelector(".pad").style.backgroundColor="pink";
+document.querySelector(".pad").style.borderRadius="20px";
+document.querySelector("h1").style.borderLeft="2px dotted green";
+document.querySelector("h1").style.borderRadius="20px";
