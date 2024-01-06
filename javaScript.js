07920d18e74016edb5d771c24c58862514009f9d
kowshik <kowshikganesh2002@gmail.com>
2024-01-06 20:53:20 +0530

    changes made

--- javaScript.js
+++ javaScript.js
@@ -3,3 +3,7 @@ document.querySelector(".pad").style.backgroundColor="pink";
 document.querySelector(".pad").style.borderRadius="20px";
 document.querySelector("h1").style.borderLeft="2px dotted green";
 document.querySelector("h1").style.borderRadius="20px";
+const h3 = document.querySelectorAll("h3");
+for(let i=0; i< h3.length; i++){
+    h3[i].style.color="Blue";
+}
